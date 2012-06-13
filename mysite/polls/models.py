657af2e435ee222b96f4244cfd1e094bf4bb8b9e
Patrick <conway.pd@gmail.com>
2012-06-12 23:17:37 -0300

    Update master

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,11 +4,12 @@ import datetime
 
 # Create your models here.
 class Poll(models.Model):
+    #I am a minor change
     question = models.CharField(max_length=200)
     pub_date =models.DateTimeField('date published')
 
     def opened_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1) 
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
     def __unicode__(self):
         return self.question
